f9cf6a22461db96b10f88bfd471cc4ac708260cf
Vishal <wadhwaniv1@gmail.com>
2021-11-18 02:39:38 +0530

    iwp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useReducer } from "react";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as tf from '@tensorflow/tfjs'
+import "./App.css";
+
+//stateMachine
+const stateMachine = {
+  initial: "initial",
+  states: {
+    initial: { on: { next: "loadingModel" } },
+    loadingModel: { on: { next: "modelReady" } },
+    modelReady: { on: { next: "imageReady" } },
+    imageReady: { on: { next: "identifying" }, showImage: true },
+    identifying: { on: { next: "complete" } },
+    complete: { on: { next: "modelReady" }, showImage: true, showResults: true }
+  }
+};
 
 function App() {
+  tf.setBackend("cpu");
+
+  const [results, setResults] = useState([]);
+  const [imageURL, setImageURL] = useState(null);
+  const [model, setModel] = useState(null);
+  const imageRef = useRef();
+  const inputRef = useRef();
+
+  const reducer = (state, event) =>
+  stateMachine.states[state].on[event] || stateMachine.initial;
+
+  const [appState, dispatch] = useReducer(reducer, stateMachine.initial);
+  const next = () => dispatch("next");
+
+  const loadModel = async () => {
+    next();
+    const model = await mobilenet.load();
+    setModel(model);
+    next();
+  };
+
+  const identify = async () => {
+    next();
+    const results = await model.classify(imageRef.current);
+    setResults(results);
+    next();
+  };
+
+  const reset = async () => {
+    setResults([]);
+    next();
+  };
+
+  const upload = () => inputRef.current.click();
+
+  const handleUpload = event => {
+    const { files } = event.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(event.target.files[0]);
+      setImageURL(url);
+      next();
+    }
+  };
+
+  const actionButton = { 
+    initial: { action: loadModel, text: "Load Model" },
+    loadingModel: { text: "Loading Model..." },
+    modelReady: { action: upload, text: "Upload Image" },
+    imageReady: { action: identify, text: "Identify Breed" },
+    identifying: { text: "Identifying..." },
+    complete: { action: reset, text: "Reset" }
+  };
+
+  const { showImage, showResults } = stateMachine.states[appState];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showImage && <img src={imageURL} alt="upload-preview" ref={imageRef} />}
+      <input
+        type="file"
+        accept="image/*"
+        capture="camera"
+        onChange={handleUpload}
+        ref={inputRef}
+      />
+      {showResults && (
+        <ul>
+          {results.map(({ className, probability }) => (
+            <li key={className}>{`${className}: %${(probability * 100).toFixed(
+              2
+            )}`}</li>
+          ))}
+        </ul>
+      )}
+      <button onClick={actionButton[appState].action || (() => {})}>
+        {actionButton[appState].text}
+      </button>
     </div>
   );
 }
